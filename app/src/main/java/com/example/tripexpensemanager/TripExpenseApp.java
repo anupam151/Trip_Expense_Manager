acdff1851061f5a6d8576fba3735b4e0520c16a5
Anupam <anupamdas151@gmail.com>
2026-06-27 16:46:14 +0530

    initial commit

---
+++ app/src/main/java/com/example/tripexpensemanager/TripExpenseApp.java
@@ -0,0 +1,14 @@
+package com.example.tripexpensemanager;
+
+import android.app.Application;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class TripExpenseApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // This single line forces the entire app to strictly run in Light Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+}
